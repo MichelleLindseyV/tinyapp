1473208bcdb029bd24c52c54b6d8aded6972b091
Michelle Verge <michellelverge@gmail.com>
2021-04-12 20:44:33 +0000

    added route to display urlDatabase object

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const urlDatabse = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
