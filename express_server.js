c72b75dbfaa737677487312a85c990f41cfe5f12
Michelle Verge <michellelverge@gmail.com>
2021-04-13 01:40:24 +0000

    created header partial, updated ejs files to includ header partial

--- express_server.js
+++ express_server.js
@@ -15,7 +15,6 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  console.log("hello!");
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
